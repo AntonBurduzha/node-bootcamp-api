32fb483f623544e71c135445fd911bf569ec2ec5
antonburduzha <anton.burduzha@hotmail.com>
2020-10-20 14:06:00 +0300

    added file upload, cost aggregation, population for bootcamp and course

---
+++ middleware/advancedResults.js
@@ -0,0 +1,60 @@
+const advancedResults = (model, populate) => async (req, res, next) => {
+    let query;
+
+    const reqQuery = { ...req.query };
+
+    // Loop over excluded fields and delete them from reqQuery
+    ['select', 'sort', 'page', 'limit'].forEach(param => delete reqQuery[param]);
+
+    let queryStr = JSON.stringify(reqQuery);
+
+    // Create operators ($gt, $gte, etc))
+    queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
+
+    query = model.find(JSON.parse(queryStr));
+
+    // Select Fields
+    if (req.query.select) {
+        const fields = req.query.select.split(',').join(' ');
+        query = query.select(fields);
+    }
+
+    // Sort fields
+    if (req.query.sort) {
+        const sortBy = req.query.sort.split(',').join(' ');
+        query = query.sort(sortBy);
+    } else {
+        query = query.sort('-createdAt');
+    }
+
+    // Pagination
+    const page = parseInt(req.query.page, 10) || 1;
+    const limit = parseInt(req.query.limit, 10) || 10;
+    // Specify offset for collection
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+    const total = await model.countDocuments();
+
+    query = query.skip(startIndex).limit(limit);
+
+    if (populate) {
+        query = query.populate(populate);
+    }
+
+    const results = await query;
+
+    // Pagination result
+    const pagination = {};
+    if (endIndex < total) {
+        pagination.next = { page: page + 1, limit };
+    }
+    if (startIndex > 0) {
+        pagination.prev = { page: page - 1, limit };
+    }
+
+    res.advancedResults = { success: true, count: results.length, data: results, pagination };
+
+    next();
+};
+
+module.exports = advancedResults;
