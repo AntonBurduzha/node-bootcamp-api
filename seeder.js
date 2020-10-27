2391f0a9673e121c6cc14289f522330c307cfc13
antonburduzha <anton.burduzha@hotmail.com>
2020-10-27 16:59:17 +0200

    added ownership for bootcamps courses and forgor pass

--- seeder.js
+++ seeder.js
@@ -9,6 +9,7 @@ dotenv.config({ path: './config/config.env' });
 // Load models
 const Bootcamp = require('./models/Bootcamp');
 const Course = require('./models/Course');
+const User = require('./models/User');
 
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
@@ -19,6 +20,8 @@ mongoose.connect(process.env.MONGO_URI, {
 });
 
 // Read JSON files
+const users = JSON.parse(fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8'));
+
 const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8'));
 
 const courses = JSON.parse(fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8'));
@@ -26,6 +29,7 @@ const courses = JSON.parse(fs.readFileSync(`${__dirname}/_data/courses.json`, 'u
 // Import into DB
 const importData = async () => {
     try {
+        await User.create(users);
         await Bootcamp.create(bootcamps);
         await Course.create(courses);
 
@@ -41,7 +45,7 @@ const deleteData = async () => {
     try {
         await Bootcamp.deleteMany();
         await Course.deleteMany();
-
+        await User.deleteMany();
         console.log('Data Destroyed...'.red.inverse);
         process.exit();
     } catch (err) {
